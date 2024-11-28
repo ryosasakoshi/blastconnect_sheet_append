a4041d9ce9b96932a6ab711e3d94acde6deb3901
ryosasakoshi <40412063+ryosasakoshi@users.noreply.github.com>
2024-11-28 13:02:48 +0900

    Update blastconnect_sheet_append.py

--- blastconnect_sheet_append.py
+++ blastconnect_sheet_append.py
@@ -177,7 +177,6 @@ def process_excel_file(uploaded_file):
         if all_data:
             # すべてのデータを結合
             merged_df = pd.concat(all_data, ignore_index=True)
-            merged_df = merged_df[merged_df["スイング条件"]=="In Game"]
 
             # 日付でソート
             try:
